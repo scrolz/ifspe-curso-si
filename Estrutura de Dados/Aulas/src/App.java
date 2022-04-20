0f14057676fbc395e4134570d387e165090f904e
Walter José <walterjosecruz@gmail.com>
2022-04-20 17:26:09 -0300

    Implementações no App.java e Vetor.java

--- Estrutura de Dados/Aulas/src/App.java
+++ Estrutura de Dados/Aulas/src/App.java
@@ -2,28 +2,36 @@
 public class App {
 
 	public static void main(String[] args) {
-		Vetor vetor = new Vetor(10);
+		Vetor vetor = new Vetor(4);
 		
-		System.out.println("Tamanho: " + vetor.getTamanho());
+		System.out.println("Tamanho inicial: " + vetor.getTamanho());
+		System.out.println();
 		
-		vetor.adiciona("Hornet");
-		vetor.adiciona("Miragem");
-		vetor.adiciona("Heaven");
+		vetor.adiciona("Dust II");
+		vetor.adiciona("Mirage");
+		vetor.adiciona("Overpass");
+		vetor.adiciona("Bomb A");
+		vetor.adiciona("Bomb B");
+		vetor.adiciona("Bomb C");
+		
 		System.out.println("Inserindo novo elemento...");
 		
-		System.out.println("Tamanho: " + vetor.getTamanho2());
-		
 		System.out.println(vetor);
-		System.out.println("Buscando o valor da pos[2]: " + vetor.busca(2));
+		
+		System.out.println("Tamanho atual: " + vetor.getTamanho2());
+		System.out.println();
+		
+		System.out.println("Buscando o valor da posição 2...");
+		System.out.println("Elemento encontrado: " + vetor.busca(2));
+		System.out.println();
 		
 		//System.out.println("Buscando elemento: " + vetor.verifica("Mirage"));
 		
-		if(vetor.verifica("Miragem")) {
+		System.out.println("Buscando valor pelo nome...");
+		if(vetor.verifica("Mirage")) {
 			System.out.println("Valor Encontrado!");
 		}else {
-			System.out.println("Valor não encontrador!");
+			System.out.println("Valor não encontrado!");
 		}
-		
 	}
-
 }
--- Estrutura de Dados/Aulas/src/Vetor.java
+++ Estrutura de Dados/Aulas/src/Vetor.java
@@ -16,13 +16,15 @@ public class Vetor {
 			}
 		}
 	}*/
-	public boolean adiciona(String elemento) {
+	public void adiciona(String elemento) {
 		if(this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
-			this.tamanho++;
-			return true;
+			
+		}else {
+			this.aumentaCapacidade();
+			this.elementos[this.tamanho] = elemento;
 		}
-		return false;
+		this.tamanho++;
 	}
 	
 	//Aula 03/04
@@ -79,5 +81,39 @@ public class Vetor {
 			}
 		}
 		return false;
+	}
+	
+	public int verificaPosicao(String elemento) {
+		for(int i = 0; i < this.tamanho; i++) {
+			if(this.elementos[i].equalsIgnoreCase(elemento)) {
+				return i;
+			}
+		}
+		return -1;
+	}	
+	//Aula 05
+	public boolean adicionaPosicao(int posicao, String elemento) {
+		
+		if(!((posicao >= 0) && (posicao <= this.tamanho))) {
+			throw new IllegalArgumentException("Posição Inválida!");
+		}
+		for(int i = this.tamanho - 1; i >= posicao; i--) {
+			this.elementos[i + 1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+		return true;
+	}
+	
+	//Aula 06
+	public void aumentaCapacidade() {
+		if(this.tamanho == this.elementos.length) {
+			String[] elementosNovos = new String[this.elementos.length * 2];
+			
+			for(int i = 0; i < this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
 	}
 }
